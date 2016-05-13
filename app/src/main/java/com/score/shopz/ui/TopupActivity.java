a5c75e90a01ec5f8cd4972f2500900f5eb67d0af
cpsuduwella <cpsuduwella@gmail.com>
2016-05-13 23:39:11 +0800

    setup Topup nfc

--- app/src/main/java/com/score/shopz/ui/TopupActivity.java
+++ app/src/main/java/com/score/shopz/ui/TopupActivity.java
@@ -2,11 +2,25 @@ package com.score.shopz.ui;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Parcelable;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.score.senz.ISenzService;
 import com.score.shopz.R;
 
 /**
@@ -19,6 +33,13 @@ public class TopupActivity extends Activity {
     // custom type face
     private Typeface typeface;
 
+    // deals with NFC
+    private NfcAdapter nfcAdapter;
+    private PendingIntent nfcPendingIntent;
+    private IntentFilter[] nfcIntentFilters;
+    private String[][] nfcTechLists;
+
+
     /*
     * {@inheritDoc}
     */
@@ -30,7 +51,7 @@ public class TopupActivity extends Activity {
 
         initUi();
         initActionBar();
-//        initNfc();
+        initNfc();
 //        initDrawer();
 //        initDrawerUser();
 //        initDrawerList();
@@ -58,4 +79,84 @@ public class TopupActivity extends Activity {
         actionBarTitle.setTextColor(getResources().getColor(R.color.white));
         actionBarTitle.setTypeface(typeface);
     }
+
+    /**
+     * Initialize NFC components
+     */
+    private void initNfc() {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "[ERROR] No NFC supported", Toast.LENGTH_LONG).show();
+        } else {
+            // create an intent with tag data and deliver to this activity
+            nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+            // set an intent filter for all MIME data
+            IntentFilter nfcDiscoveredIntentFilter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+            try {
+                nfcDiscoveredIntentFilter.addDataType("text/plain");
+                nfcIntentFilters = new IntentFilter[]{nfcDiscoveredIntentFilter};
+            } catch (Exception e) {
+                Log.e("TagDispatch", e.toString());
+            }
+
+            // tech list
+            nfcTechLists = new String[][]{new String[]{NfcF.class.getName()}};
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // enable foreground dispatch
+        if (nfcAdapter != null) {
+            nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, nfcIntentFilters, nfcTechLists);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // disable foreground dispatch
+        if (nfcAdapter != null)
+            nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        String s = action + "\n\n" + tag.toString();
+        Log.d(TAG, "tag... " + s);
+
+        // parse through all NDEF messages and their records and pick text type only
+        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (data != null) {
+            NdefMessage message = (NdefMessage) data[0];
+            String amount = new String(message.getRecords()[0].getPayload());
+            Log.d(TAG, "----------------- " + amount);
+
+            // launch pay activity
+            Intent mapIntent = new Intent(this, PayActivity.class);
+            mapIntent.putExtra("EXTRA", amount);
+            startActivity(mapIntent);
+            overridePendingTransition(R.anim.bottom_in, R.anim.stay_in);
+
+        }
+    }
+
+
 }
